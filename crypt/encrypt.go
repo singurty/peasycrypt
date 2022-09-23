fdb2da3f2cb58e6a7db6a600aee389351b87f128
singurty <singurty@gmail.com>
2022-09-23 20:17:37 +0545

    traverse directory

--- crypt/encrypt.go
+++ crypt/encrypt.go
@@ -2,8 +2,13 @@ package crypt
 
 import (
 	"fmt"
+	"io/fs"
+	"path/filepath"
 )
 
 func EncryptDirectory(srcPath, dstPath string) {
-	fmt.Printf("i will encrypt %v and store at %v ok\n", srcPath, dstPath)
+	filepath.WalkDir(srcPath, func(path string, d fs.DirEntry, err error) error {
+		fmt.Printf("current directory: %v\n", path)
+		return nil
+	})
 }
