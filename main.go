11848ae926dc0d68c592f60fa05b6495c36c43de
singurty <singurty@gmail.com>
2022-09-23 15:50:07 +0545

    encrypt and decrypt files

--- main.go
+++ main.go
@@ -15,25 +15,63 @@ func check (e error) {
 	}
 }
 
-func main() {
-	password := "pppppppppppppppppppppppppppppppp"
-	data, err := os.ReadFile("test/plain")
-	check(err)
-
+func encrypt(data []byte, password string) ([]byte, error) {
 	if len(data) % aes.BlockSize != 0 {
 		panic("blocksize mismatch")
 	}
 	block, err := aes.NewCipher([]byte (password))
-	check(err)
+	
+	if err != nil {
+		return nil, err
+	}
 
 	cipertext := make([]byte, aes.BlockSize + len(data))
 	iv := cipertext[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	mode := cipher.NewCBCEncrypter(block, iv)
 	mode.CryptBlocks(cipertext[aes.BlockSize:], data)
 
-	fmt.Printf("%x\n", cipertext)
+	return cipertext, nil
+}
+
+func decrypt(data []byte, password string) ([]byte, error) {
+	block, err := aes.NewCipher(([]byte (password)))
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) < aes.BlockSize {
+		panic("encrypted data too short")
+	}
+	iv := data[:aes.BlockSize]
+	ciphertext := data[aes.BlockSize:]
+
+	if len(ciphertext) % aes.BlockSize != 0 {
+		panic("ciphertext is not a multiple of the block size")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext, ciphertext)
+	return ciphertext, nil
+}
+
+func main() {
+	password := "pppppppppppppppppppppppppppppppp"
+	data, err := os.ReadFile("test/plain")
+	check(err)
+
+	encrypted_data, err := encrypt(data, password)
+	check(err)
+
+	err = os.WriteFile("test/plain_crypt", encrypted_data, 0664)
+	check(err)
+
+	encrypted_file, err := os.ReadFile("test/plain_crypt")
+	check(err)
+
+	decrypted_data, err := decrypt(encrypted_file, password)
+	fmt.Printf("Decrypted: %v\n", string(decrypted_data))
 }
