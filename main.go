3c31b92cd4fee0ee983f6468b9686c89e6cc091f
Shirish Pokhrel <singurty@gmail.com>
2022-10-03 11:15:56 +0545

    clean main.go

--- main.go
+++ main.go
@@ -1,21 +1,8 @@
 package main
 
 import (
-_	"crypto/rand"
-_	"os"
-
 	"github.com/singurty/peasycrypt/cmd"
-
-_	"golang.org/x/crypto/scrypt"
 )
-
-var TEST_FILE = "test/video.mkv"
-
-func check (e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 
 func main() {
 	cmd.Execute()
